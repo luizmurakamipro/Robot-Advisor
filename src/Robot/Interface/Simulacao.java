2a2734242ef12e2f93b5151f1d49f1088cf5bff9
Luiz Paulo <>
2019-09-11 19:45:28 -0300

    janelas Login, Cadastro, Investir, Principal, Questionario, Relatorio, Simulação.

--- src/Robot/Interface/Simulacao.java
+++ src/Robot/Interface/Simulacao.java
@@ -14,7 +14,7 @@ public class Simulacao extends javax.swing.JFrame {
     /**
      * Creates new form Simulacao
      */
-    public Simulacao() {
+    public Simulacao() { 
         initComponents();
     }
 
