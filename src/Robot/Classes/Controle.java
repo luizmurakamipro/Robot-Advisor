9b00fa134fb839146be6b2ccfa83ef3fbed5d5b0
Luiz <luizhm.murakami@univem.edu.br>
2019-09-11 20:50:00 -0300

    Updated.

---
+++ src/Robot/Classes/Controle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Robot.Classes;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.jdesktop.observablecollections.ObservableCollections;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Controle {
+    public static List<Usuario> pUser;
+    
+    public Controle()
+    {
+        pUser = new LinkedList<>(); 
+    }
+}
